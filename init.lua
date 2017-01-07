f5569e86608b2d2f16368bdf47b25c544a948c32
yzelast <yzelast@gmail.com>
2017-01-07 01:34:23 -0300

    beds added,ladder recipes fixed

--- init.lua
+++ init.lua
@@ -4,5 +4,7 @@ dofile(minetest.get_modpath("wood_plus") .. "/trapdoors.lua")
 dofile(minetest.get_modpath("wood_plus") .. "/doors.lua")
 dofile(minetest.get_modpath("wood_plus") .. "/signs.lua")
 dofile(minetest.get_modpath("wood_plus") .. "/beds.lua")
+dofile(minetest.get_modpath("wood_plus") .. "/craftitems.lua")
+dofile(minetest.get_modpath("wood_plus") .. "/crafts.lua")
 
 
